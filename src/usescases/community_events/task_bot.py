09f0dbb3cf006c4ff5a1281bb2b5d123fa0154a8
Nelson Nobre <technobre@gmail.com>
2025-09-23 23:54:54 +0100

    refactor: enhance logging and improve error messages

--- src/usescases/community_events/task_bot.py
+++ src/usescases/community_events/task_bot.py
@@ -39,56 +39,66 @@ class CommunityEventsTaskBot(commands.Cog):
     @tasks.loop(minutes=15) # Every 15 minutes
     async def notify_upcoming_events(self):
         try:
+            logger.debug('[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Notifying upcoming events...')
+
             channel = self.bot.get_channel(self.community_events_channel_id)
             if channel is None:
-                raise ValueError(f'Could not find channel with ID {self.community_events_channel_id}')
+                raise ValueError(f'Could not find channel with Id "{self.community_events_channel_id}"')
 
             time_zone = ZoneInfo('America/Sao_Paulo')
             now = datetime.now(time_zone)
 
             # only send notifications after 8am
             if now.hour < 8:
+                logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Not sending notifications before 8am (São Paulo Time) - current hour: {now.strftime("%Y-%m-%d %H:%M:%S")}')
                 return
 
-            logger.debug('[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Notifying upcoming events...')
-
             upcoming_events = community_events_dao.get_upcoming_events(now)
+
+            logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Found {len(upcoming_events)} upcoming events to check for notifications')
 
             for event in upcoming_events:
                 reminder_time = event.reminder_time()
                 if not reminder_time:
+                    logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] The event: "{event.title}" is not in a timewindow for notification')
                     continue
 
                 if reminder_time == ReminderTime.A_HOUR:
                     if event.a_hour_notify:
+                        logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] The event: "{event.title}" already had the "{reminder_time.name}" notification sent')
                         continue
                     event.a_hour_notify = True
 
                 elif reminder_time == ReminderTime.A_DAY:
                     if event.a_day_notify:
+                        logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] The event: "{event.title}" already had the "{reminder_time.name}" notification sent')
                         continue
                     event.a_day_notify = True
 
                 elif reminder_time == ReminderTime.THREE_DAYS:
                     if event.three_days_notify:
+                        logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] The event: "{event.title}" already had the "{reminder_time.name}" notification sent')
                         continue
                     event.three_days_notify = True
 
                 elif reminder_time == ReminderTime.A_WEEK:
                     if event.a_weekly_notify:
+                        logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] The event: "{event.title}" already had the "{reminder_time.name}" notification sent')
                         continue
                     event.a_weekly_notify = True
+
+                logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] The event: "{event.title}" will now notify {reminder_time.name} in advance')
 
                 embed = event_formatter.format_event_notification(event)
                 await channel.send(embed=embed)
 
                 community_events_dao.update(event)
 
-                logger.info(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Notifying event: {event.title} at {event.start_datetime}')
+                logger.info(f'[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Notifying event: "{event.title}" at {event.start_datetime} - Reminder: {reminder_time.name}')
                 break
 
         except Exception:
-            logger.exception('[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Error in notify task:')
+            logger.exception('[BOT][TASK][COMMUNITY EVENTS][NOTIFY] Error in notify task')
 
     @tasks.loop(hours=3) # Every 3 hours
     async def update_community_events_task(self):
@@ -135,9 +145,11 @@ class CommunityEventsTaskBot(commands.Cog):
 
     async def __create_discord_event(self, event: CommunityEvent) -> Optional[str]:
         try:
+            logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Creating discord event for "{event.title}"')
+
             channel = self.bot.get_channel(self.community_events_channel_id)
             if channel is None:
-                raise ValueError(f'Could not find channel with ID {self.community_events_channel_id}')
+                raise ValueError(f'Could not find channel with Id "{self.community_events_channel_id}"')
 
             guild = channel.guild
 
@@ -158,7 +170,7 @@ class CommunityEventsTaskBot(commands.Cog):
                 entity_type = discord.EntityType.external,
                 location = event.discord_event_location())
 
-            logger.info(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Created discord event for "{event.title}" with Id {discord_event.id}')
+            logger.info(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Created discord event for "{event.title}" with Id "{discord_event.id}"')
 
             return str(discord_event.id)
         except Exception:
@@ -168,14 +180,16 @@ class CommunityEventsTaskBot(commands.Cog):
 
     async def __delete_discord_event(self, discord_event_id: str) -> None:
         try:
+            logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Deleting discord event Id "{discord_event_id}"')
+
             channel = self.bot.get_channel(self.community_events_channel_id)
             if channel is None:
-                raise ValueError(f'Could not find channel with ID {self.community_events_channel_id}')
+                raise ValueError(f'Could not find channel with Id "{self.community_events_channel_id}"')
 
             guild = channel.guild
             discord_event = await guild.fetch_scheduled_event(int(discord_event_id))
             if discord_event:
                 await discord_event.delete()
-                logger.debug(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Deleted discord event ID {discord_event_id}')
+                logger.info(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Deleted discord event Id "{discord_event_id}"')
         except Exception:
-            logger.exception(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Failed to delete discord event ID {discord_event_id}')
+            logger.exception(f'[BOT][TASK][COMMUNITY EVENTS][DISCORD] Failed to delete discord event Id "{discord_event_id}"')
