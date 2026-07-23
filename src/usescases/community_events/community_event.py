ba36795f30335c21818128a78080e9978db05d57
Nelson Nobre <technobre@gmail.com>
2026-07-23 20:43:24 +0100

    fixcommunity_event): prevent publishing session link in discord event location

--- src/usescases/community_events/community_event.py
+++ src/usescases/community_events/community_event.py
@@ -54,8 +54,8 @@ class CommunityEvent:
         return None
 
     def discord_event_location(self) -> str:
-        if self.session_link:
-            return self.session_link
+        # if self.session_link:
+        #     return self.session_link
         if self.has_recording_link() and self.is_live_event():
             return self.recording_link
         return self.event_details_url()
