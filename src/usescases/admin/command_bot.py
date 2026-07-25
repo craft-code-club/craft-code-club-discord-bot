62895126fe9c0261bf2fa9479b03ad678bcca3df
Nelson Nobre <technobre@gmail.com>
2026-07-25 20:08:47 +0100

    feat(commands): add events command to list upcoming events for admins

--- src/usescases/admin/command_bot.py
+++ src/usescases/admin/command_bot.py
@@ -3,6 +3,7 @@ from .info_command import InfoCommandBot
 from .add_role_command import AddRoleCommandBot
 from .event_session_link_command import EventSessionLinkCommandBot
 from .server_status_command import ServerStatusCommandBot
+from .events_command import EventsCommandBot, EventDetailsCommandBot
 
 
 async def setup(bot):
@@ -11,3 +12,5 @@ async def setup(bot):
     await bot.add_cog(AddRoleCommandBot(bot))
     await bot.add_cog(EventSessionLinkCommandBot(bot))
     await bot.add_cog(ServerStatusCommandBot(bot))
+    await bot.add_cog(EventsCommandBot(bot))
+    await bot.add_cog(EventDetailsCommandBot(bot))
